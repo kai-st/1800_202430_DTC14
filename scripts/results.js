d4a52f84c1389edb6bcc74b6a9fa8a82a88caf83
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-11-07 07:44:19 -0800

    Get user subscriptions in results.js

--- scripts/results.js
+++ scripts/results.js
@@ -1,10 +1,8 @@
-const addNotificationsIcon = "&#xe399;";
-const removeNotificationsIcon = "&#xe7f6;";
-const editNotificationsIcon = "&#xe525;";
+const ADD_NOTIFICATION_ICON = "&#xe399;";
+const REMOVE_NOTIFICATION_ICON = "&#xe7f6;";
+const EDIT_NOTIFICATION_ICON = "&#xe525;";
 
-let loggedIn = true;
-
-let topic = "Topic";
+let topic = "COVID-19";
 
 const dummyResults = [
     {
@@ -96,128 +94,157 @@ const dummyResults = [
     },
 ];
 
-const loadResults = () => {
+const loadResults = async () => {
     console.log("loading");
-    if (dummyResults?.length == 0) {
-        return;
-    }
 
     $("#results-wrapper").empty();
 
-    dummyResults.forEach((result) => {
-        let subpages = "";
+    firebase.auth().onAuthStateChanged(async (user) => {
+        let loggedIn = !!user;
 
-        result.subpages.forEach((subpage) => {
-            const subpageAccordionItem = `<div class="subpage-item accordion-item">
-                                <div class="subpage-header accordion-header">
-                                    <h3>${subpage.subpageTitle}</h3>
-                                    ${
-                                        loggedIn
-                                            ? `<button
-                                        type="button"
-                                        class="subpage-subscribe subscribe btn"
+        const userSubscriptionPaths = await getUserSubscriptionPaths(user.uid);
+
+        dummyResults.forEach((result) => {
+            let subpages = "";
+
+            result.subpages.forEach((subpage) => {
+                const subpageAccordionItem = `<div class="subpage-item accordion-item">
+                                    <div class="subpage-header accordion-header">
+                                        <h3>${subpage.subpageTitle}</h3>
+                                        ${
+                                            loggedIn
+                                                ? `<button
+                                            type="button"
+                                            class="subpage-subscribe subscribe btn"
+                                        >
+                                            <span class="material-icons"
+                                                >&#xe399;
+                                            </span>
+                                        </button>`
+                                                : ""
+                                        }
+                                        <button
+                                            class="accordion-button custom collapsed"
+                                            type="button"
+                                            data-bs-toggle="collapse"
+                                            data-bs-target="#embed${subpage.id}"
+                                            aria-expanded="false"
+                                            aria-controls="embed${subpage.id}"
+                                        ></button>
+                                    </div>
+                                    <div
+                                        id="embed${subpage.id}"
+                                        class="accordion-collapse collapse"
                                     >
-                                        <span class="material-icons"
-                                            >&#xe399;
-                                        </span>
-                                    </button>`
-                                            : ""
-                                    }
-                                    <button
-                                        class="accordion-button custom collapsed"
-                                        type="button"
-                                        data-bs-toggle="collapse"
-                                        data-bs-target="#embed${subpage.id}"
-                                        aria-expanded="false"
-                                        aria-controls="embed${subpage.id}"
-                                    ></button>
-                                </div>
-                                <div
-                                    id="embed${subpage.id}"
-                                    class="accordion-collapse collapse"
-                                >
-                                    <div class="embed">
-                                        <div class="embed-content">
-                                            ${subpage.content}
+                                        <div class="embed">
+                                            <div class="embed-content">
+                                                ${subpage.content}
+                                            </div>
+                                            <a
+                                                href="${subpage.subpageUrl}"
+                                                class="subpage-link"
+                                                >Visit Page</a
+                                            >
                                         </div>
-                                        <a
-                                            href="${subpage.subpageUrl}"
-                                            class="subpage-link"
-                                            >Visit Page</a
-                                        >
                                     </div>
-                                </div>
-                            </div>`;
+                                </div>`;
 
-            subpages += subpageAccordionItem;
+                subpages += subpageAccordionItem;
+            });
+
+            const sourceAccordionItem = `<section class="source-block" data-source-id="${
+                result.id
+            }">
+                                    <div class="source">
+                                        <h2>
+                                            <span class="d-none d-sm-inline">From: </span
+                                            ><a class="source-link" href="${
+                                                result.sourceUrl
+                                            }"
+                                                ><img
+                                                    src="${
+                                                        result.sourceLogoUrl
+                                                    }"
+                                                    class="source-logo"
+                                                />Domain</a
+                                            >
+                                        </h2>
+                                        <div class="results-tag">
+                                            (${result.subpages.length} results)
+                                        </div>
+                                        ${
+                                            loggedIn
+                                                ? `<div class="btn-group dropup">
+                                                    <button
+                                                        type="button"
+                                                        class="source-subscribe subscribe btn dropdown-toggle"
+                                                        data-bs-toggle="dropdown"
+                                                        aria-expanded="false"
+                                                    >
+                                                        <span class="material-icons">&#xe399; </span>
+                                                    </button>
+                                                    <ul class="dropdown-menu dropdown-menu-end">
+                                                        <li>
+                                                            <button
+                                                                class="subscribe-topic dropdown-item"
+                                                                type="button"
+                                                            >
+                                                                Subscribe to updates on ${result.topic}
+                                                            </button>
+                                                        </li>
+                                                        <li>
+                                                            <button
+                                                                class="subscribe-all dropdown-item"
+                                                                type="button"
+                                                            >
+                                                                Subscribe to all updates from ${result.sourceName}
+                                                            </button>
+                                                        </li>
+                                                    </ul>
+                                                </div>`
+                                                : ""
+                                        }                                    
+                                    </div>
+                                    <div
+                                        class="subpages accordion accordion-flush ms-md-5"
+                                    >
+                                    ${subpages}
+                                    <button
+                                        class="view-more-btn btn btn-info"
+                                        type="button"
+                                    >
+                                        View More
+                                    </button>
+                                    </div>
+                                </section>`;
+
+            $("#results-wrapper").append(sourceAccordionItem);
         });
-
-        const sourceAccordionItem = `<section class="source-block" data-source-id="${
-            result.id
-        }">
-                                <div class="source">
-                                    <h2>
-                                        <span class="d-none d-sm-inline">From: </span
-                                        ><a class="source-link" href="${
-                                            result.sourceUrl
-                                        }"
-                                            ><img
-                                                src="${result.sourceLogoUrl}"
-                                                class="source-logo"
-                                            />Domain</a
-                                        >
-                                    </h2>
-                                    <div class="results-tag">
-                                        (${result.subpages.length} results)
-                                    </div>
-                                    ${
-                                        loggedIn
-                                            ? `<div class="btn-group dropup">
-                                                <button
-                                                    type="button"
-                                                    class="source-subscribe subscribe btn dropdown-toggle"
-                                                    data-bs-toggle="dropdown"
-                                                    aria-expanded="false"
-                                                >
-                                                    <span class="material-icons">&#xe399; </span>
-                                                </button>
-                                                <ul class="dropdown-menu dropdown-menu-end">
-                                                    <li>
-                                                        <button
-                                                            class="subscribe-topic dropdown-item"
-                                                            type="button"
-                                                        >
-                                                            Subscribe to updates on ${result.topic}
-                                                        </button>
-                                                    </li>
-                                                    <li>
-                                                        <button
-                                                            class="subscribe-all dropdown-item"
-                                                            type="button"
-                                                        >
-                                                            Subscribe to all updates from ${result.sourceName}
-                                                        </button>
-                                                    </li>
-                                                </ul>
-                                            </div>`
-                                            : ""
-                                    }                                    
-                                </div>
-                                <div
-                                    class="subpages accordion accordion-flush ms-md-5"
-                                >
-                                ${subpages}
-                                <button
-                                    class="view-more-btn btn btn-info"
-                                    type="button"
-                                >
-                                    View More
-                                </button>
-                                </div>
-                            </section>`;
-
-        $("#results-wrapper").append(sourceAccordionItem);
     });
 };
 
 loadResults();
+
+async function getUserSubscriptionPaths(userId) {
+    try {
+        if (!userId) {
+            throw new Error("No logged in user, cannot get subscriptions");
+        }
+
+        const userDoc = await db.collection("users").doc(userId).get();
+
+        if (!userDoc.exists) {
+            throw new Error("No user found in db");
+        }
+
+        const subscriptionPaths = userDoc
+            .data()
+            .subscriptions.map((subscriptionRef) => subscriptionRef.path);
+        console.log("user subscription paths", subscriptionPaths);
+
+        return subscriptionPaths;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
