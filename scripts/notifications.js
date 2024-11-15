007cf64184d800c00cce2a6f95ec62680b42bf15
Kai Steingarten <ksteingarten@my.bcit.ca>
2024-11-15 15:15:15 -0800

    Update notification queries

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -1,59 +1,55 @@
 firebase.auth().onAuthStateChanged((user) => {
-    loadNotifications(user)
-})
+    loadNotifications(user);
+});
 
+async function loadNotifications(user) {
+    db.collection("users").doc(user.uid).update("notificationsRead", true);
 
-async function loadNotifications(user)
-{
-    db.collection("users").doc("user.uid").update("notificationsRead", true)
-    
-    notificationCollection = await db.collection("users").doc("user.uid").collection("notifications").get()
-    notificationIds = []
-    notifications = []
-    for(notification of notificationCollection.docs)
-    {
-        notificationData = await notification.data()
-        notificationIds.push(notificationData.subpages[0])
+    notificationCollection = await db
+        .collection("users")
+        .doc(user.uid)
+        .collection("notifications")
+        .get();
+    notificationIds = [];
+    notifications = [];
+    for (notification of notificationCollection.docs) {
+        notificationData = await notification.data();
+        notificationIds.push(notificationData.subpages[0]);
     }
 
-    sources = db.collection("sources")
-    sourcesSnapshot = await db.collection("sources").get()
+    sources = db.collection("sources");
+    sourcesSnapshot = await db.collection("sources").get();
 
-    for(source of sourcesSnapshot.docs)
-    {
-        sourcesSubpagesSnapshot = await source.ref.collection("subpages").get()
-        for(notificationId of notificationIds) {
-            
-            sourcesSubpagesSnapshot.forEach(subpage => {
-                if (subpage.id == notificationId )
-                {
-                    notifications.push(subpage)
+    for (source of sourcesSnapshot.docs) {
+        sourcesSubpagesSnapshot = await source.ref.collection("subpages").get();
+        for (notificationId of notificationIds) {
+            sourcesSubpagesSnapshot.forEach((subpage) => {
+                if (subpage.id == notificationId) {
+                    notifications.push(subpage);
                 }
-                });
+            });
         }
     }
 
-    for (notification of notifications)
-    {
-        displayNotification({...notification.data(), id: notification.id})
+    for (notification of notifications) {
+        displayNotification({ ...notification.data(), id: notification.id });
     }
 }
 
-async function displayNotification(doc)
-{
-    sourceID = doc.sourceID
-    source = await db.collection("sources").doc(sourceID).get()
-    sourceData = source.data()
+async function displayNotification(doc) {
+    sourceID = doc.sourceID;
+    source = await db.collection("sources").doc(sourceID).get();
+    sourceData = source.data();
 
-    logo_URL = sourceData.sourceLogoUrl
+    logo_URL = sourceData.sourceLogoUrl;
 
-    new_URL = new URL("article_template.html", window.location.href)
-    new_URL.searchParams.set('id', doc.id) 
+    new_URL = new URL("article_template.html", window.location.href);
+    new_URL.searchParams.set("id", doc.id);
 
-    title = doc.subpageTitle
-    byline = doc.subpageSummary
+    title = doc.subpageTitle;
+    byline = doc.subpageSummary;
 
-    notifications_section = document.getElementById("notifications")
+    notifications_section = document.getElementById("notifications");
 
     notifications_section.innerHTML += `<div class="article-preview">
     <div class="article-preview-title" style="display: flex; flex-direction: row; gap: 20px;">
@@ -73,27 +69,24 @@ async function displayNotification(doc)
           </span>
         </a>
       </div>
-    </div>`
+    </div>`;
 }
 
-async function printNewsArticle(doc)
-{
-    sourceID = doc.sourceID
-    source = await db.collection("sources").doc(sourceID).get()
-    sourceData = source.data()
+async function printNewsArticle(doc) {
+    sourceID = doc.sourceID;
+    source = await db.collection("sources").doc(sourceID).get();
+    sourceData = source.data();
 
-    logo_URL = sourceData.sourceLogoUrl
-   
-   
-    new_URL = new URL("article_template.html", window.location.href)
-    new_URL.searchParams.set('id', doc.id) 
+    logo_URL = sourceData.sourceLogoUrl;
 
+    new_URL = new URL("article_template.html", window.location.href);
+    new_URL.searchParams.set("id", doc.id);
 
-    title = doc.subpageTitle
-    byline = doc.subpageSummary
-    
-    news_section = document.getElementById("news")
-    
+    title = doc.subpageTitle;
+    byline = doc.subpageSummary;
+
+    news_section = document.getElementById("news");
+
     news_section.innerHTML += `<div class="article-preview">
     <div class="article-preview-title" style="display: flex; flex-direction: row; gap: 20px;">
       <img src="${logo_URL}" alt="logo" style="height: 28px; width: auto;">
@@ -112,5 +105,5 @@ async function printNewsArticle(doc)
           </span>
         </a>
       </div>
-    </div>`
+    </div>`;
 }
